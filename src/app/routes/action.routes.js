d8f3d76677badd85996d10e0822218698daf3e94
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-27 17:44:37 +0600

    docs: add comment to action routes file

--- src/app/routes/action.routes.js
+++ src/app/routes/action.routes.js
@@ -3,6 +3,7 @@ import rateLimitMiddleware from "../middlewares/rateLimitMiddleware.js";
 
 const router = express.Router();
 
+// Though it should return 201 for creation, using 200 for simplicity (as it's not creating any resource)
 router.post("/action", rateLimitMiddleware, (req, res) => {
    res.status(200).json({
       success: true,
