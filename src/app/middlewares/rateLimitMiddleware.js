5a388955f33771566fb1cb5fc5098f8d1bd97b04
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-27 17:41:14 +0600

    chore: update rate limit response message in middleware

--- src/app/middlewares/rateLimitMiddleware.js
+++ src/app/middlewares/rateLimitMiddleware.js
@@ -15,7 +15,7 @@ const rateLimitMiddleware = (req, res, next) => {
       res.status(429).json({
          success: false,
          message:
-            "You have exceeded the maximum number of requests (10 per minute). Please try again later.",
+            "You have exceeded the maximum number of requests. Please try again later.",
       });
    }
 };
