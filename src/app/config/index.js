bfa2ded7da1e2a0fa8f6596f74a339712f47c914
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-27 17:37:28 +0600

    chore: remove unnecessary code from config file

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,7 +1,5 @@
 const config = {
    port: process.env.PORT || 3000,
-   dbUrl: process.env.DB_URL,
-   jwtSecret: process.env.JWT_SECRET,
    rateLimitWindow: process.env.RATE_LIMIT_WINDOW || 60,
    rateLimitMaxRequests: process.env.RATE_LIMIT_MAX_REQUESTS || 10,
 };
