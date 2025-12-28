04d715b6b6ad5645c6b44aba0db1d395b3fbf84e
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-28 13:51:43 +0600

    feat(test): add initial rate limiter test and configure Jest for ES modules

---
+++ tests/rateLimiter.test.js
@@ -0,0 +1,16 @@
+import rateLimiter from '../src/app/services/rateLimiter.js';
+import storage from '../src/app/services/storage.js';
+
+describe("Test rateLimiter.checkLimit", () => {
+  const testIP = "123.456.789.000";
+
+  beforeEach(() => {
+    storage.setRateLimitData(testIP, []);
+  });
+
+  it("should allow requests under the limit", () => {
+    for (let i = 0; i < 10; i++) {
+      expect(rateLimiter.checkLimit(testIP)).toBe(true);
+    }
+  })
+})
